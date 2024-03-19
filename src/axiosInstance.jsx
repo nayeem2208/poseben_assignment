76c190c308abcabd4fa51d28b0a1ce6f870e3d6c
Nayeem <nayeem2281998@gmail.com>
2024-03-19 14:00:54 +0530

    validation of email added

--- src/axiosInstance.jsx
+++ src/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const baseURL = "https://poseben-backend.onrender.com/api"  ;
-const baseURL ="http://localhost:3000/api"
+const baseURL = "https://poseben-backend.onrender.com/api"  ;
+// const baseURL ="http://localhost:3000/api"
 const axiosInstance = axios.create({
   baseURL,
 });
